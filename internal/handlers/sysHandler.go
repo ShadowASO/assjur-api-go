8187cd9e303de1b75ed16ed356f0f5e7ba2fba52
ShadowASO <aldenor.oliveira@uol.com.br>
2025-10-23 17:38:45 -0300

    Versão 2.4.8:Sincronia com o cliente web

--- internal/handlers/sysHandler.go
+++ internal/handlers/sysHandler.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Versao da aplicação
-const AppVersion = "assjur2.4.7"
+const AppVersion = "assjur2.4.8"
 
 func VersionHandler(c *gin.Context) {
 	requestID := middleware.GetRequestID(c)
